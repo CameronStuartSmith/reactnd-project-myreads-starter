be4116cdd5cf911564931893ac8fb902f36ba938
Cameron Smith <cameronstuartsmith@gmail.com>
2018-04-25 13:32:53 -0500

    Fetch and Display Books, Default Selection of Shelf

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from '../utils/BooksAPI'
 import '../styles/App.css'
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 
 class BooksApp extends React.Component {
+	state = {
+		books: []
+	}
+
+	fetchBooks = () => {
+		BooksAPI.getAll().then(books => {
+			this.setState({books});
+			console.log(books);
+		});
+	}
+
+	componentDidMount() {
+		this.fetchBooks();
+	}
+
   render() {
     return (
       <div>
 		<Route exact path='/' render={() => (
-			<MainPage />
+			<MainPage books={this.state.books} />
 		)} />
 		<Route exact path='/search' render={() => (
 			<SearchPage />
